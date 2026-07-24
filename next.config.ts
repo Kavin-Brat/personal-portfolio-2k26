8da9d230006f9eaae991ffd3431d7dd330aebf82
Kavin-Brat <kavinbarath66@gmail.com>
2026-07-24 23:12:38 +0530

    config file missing added

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,12 @@ const nextConfig: NextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
   },
+  env: {
+    NEXT_PUBLIC_EMAILJS_SERVICE_ID: "service_spxi6ot",
+    NEXT_PUBLIC_EMAILJS_NOTIFICATION_TEMPLATE_ID: "template_wbkmonl",
+    NEXT_PUBLIC_EMAILJS_AUTO_REPLY_TEMPLATE_ID: "template_j3662df",
+    NEXT_PUBLIC_EMAILJS_PUBLIC_KEY: "40LAi58BPquc6anU_",
+  },
 };
 
 export default nextConfig;
